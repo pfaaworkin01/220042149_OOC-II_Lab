195699c78d29f1d52af5600c2a857a19b7bf571b
Prince Fahim Al-Araf <princefahim@iut-dhaka.edu>
2024-09-28 15:36:12 +0600

    Created the NumWordPrinter class and executed for the first time in main class

--- Task-1/src/Main.java
+++ Task-1/src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        
+        NumWordPrinter printer = new NumWordPrinter();
+
+        System.out.println(printer.printWord(2, false));
+        System.out.println(printer.printWord(11, false));
+        System.out.println(printer.printWord(13, false));
+        System.out.println(printer.printWord(22, false));
+        System.out.println(printer.printWord(286, false));
+        System.out.println(printer.printWord(1, false));
 
     }
 }
